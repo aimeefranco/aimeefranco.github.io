215110f633dad50e2a1b05b75cd9f7a969fb9ee7
AIMEE FRANCO <FRANCO.AIMEE@GMAIL.COM>
2015-01-08 13:56:47 -0600

    initial commit

--- JS/javaportfolio2.js
+++ JS/javaportfolio2.js
@@ -67,3 +67,18 @@ function navAppear(){
 		}*/
 
 $('#importThing').data("spy", "scroll")
+
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
